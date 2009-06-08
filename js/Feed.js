5b9a4ca761eae93e67f3fa2ac724cdd227902223
James A. Duncan <james@joyent.com>
2009-06-08 15:54:31 -0400

    updated feed

--- js/Feed.js
+++ js/Feed.js
@@ -18,21 +18,25 @@ Array.prototype.toFeed = function( feedOptions, withElement ) {
       mapped = withElement.apply( elem, [] );
     else
       mapped = elem;
-    myList += <entry>
-      <title>{mapped.title}</title>
-      <link rel="alternate" type="text/html" href={mapped.uri}/>
-      <id>{mapped.tag||mapped.id}</id>
-      <updated>{mapped.updated}</updated>
-      <published>{mapped.published}</published>
-      <author>
-	<name>{feedOptions.owner}</name>
-	<uri>{feedOptions.uri}</uri>
-	<email>{mapped.email||"nospam@kthnx"}</email>
-      </author>
-      <content type="xhtml" xml:lang="en" xml:base="http://diveintomark.org/">
-	<div xmlns="http://www.w3.org/1999/xhtml">{mapped.body}</div>
-      </content>
-    </entry>;
+    if ( mapped.title ) {
+      var body = new XML("<div>" + mapped.body.replace(/&nbsp;/g, " ") + "</div>");
+      system.console.log(body);
+      myList += <entry>
+	<title>{mapped.title}</title>
+	  <link rel="alternate" type="text/html" href={mapped.uri}/>
+	  <id>{mapped.tag||mapped.id}</id>
+	  <updated>{mapped.updated}</updated>
+	  <published>{mapped.created}</published>
+	  <author>
+	    <name>{feedOptions.owner}</name>
+	    <uri>{feedOptions.uri}</uri>
+	    <email>{mapped.email||"nospam@kthnx"}</email>
+	  </author>
+	  <content type="xhtml" xml:lang="en" xml:base="http://diveintomark.org/">
+	    <div xmlns="http://www.w3.org/1999/xhtml">{body}</div>
+	  </content>
+      </entry>;
+    }
   }
   feed.entry = myList;
   return ["<?xml version=\"1.0\" encoding=\"utf-8\"?>", feed.toXMLString()].join("\n");
