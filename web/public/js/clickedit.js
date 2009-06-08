9fb71a0b3253361b54bcd77f395b0baa6d8fc3cc
James A. Duncan <james@joyent.com>
2009-06-08 07:22:46 -0400

    click-to-edit is now better

--- web/public/js/clickedit.js
+++ web/public/js/clickedit.js
@@ -6,6 +6,17 @@ $(document).ready(function() {
 
   $('input.clickedit').blur( function() {
     $(this).removeClass('focused');
+    if ( $(this).val() == "" ) {
+      $(this).val("Untitled");
+      $(this).addClass('untitled');
+    }
+  });
+
+  $('input.untitled').focus( function() {
+    if ( $(this).hasClass('untitled') ) {
+      $(this).val('');
+      $(this).removeClass('untitled');
+    }
   });
 
 });
